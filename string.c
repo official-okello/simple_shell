108311bf264a036bf2eaf7d7615e1cb5bd3df6d6
official-okello <officialokello@gmail.com>
2023-05-24 22:54:56 +0300

    string.c quick fix

---
+++ string.c
@@ -0,0 +1,71 @@
+#include "shell.h"
+
+/**
+ * _strlen - returns length of a string
+ * @s: string whose length is check
+ * Return: length of string
+ */
+int _strlen(char *s)
+{
+	int i = 0;
+
+	if (!s)
+		return (0);
+
+	while (*s++)
+		i++;
+	return (i);
+}
+
+/**
+ * _strcmp - perform lexicogarphic comparison of two strangs.
+ * @s1: first strang
+ * @s2: second strang
+ * Return: negative, positive or zero
+ */
+int _strcmp(char *s1, char *s2)
+{
+	while (*s1 && *s2)
+	{
+		if (*s1 != *s2)
+			return (*s1 - *s2);
+		s1++;
+		s2++;
+	}
+	if (*s1 == *s2)
+		return (0);
+	else
+		return (*s1 < *s2 ? -1 : 1);
+}
+
+/**
+ * starts_with - check if needle starts with haystack
+ * @haystack: string to search
+ * @needle: substring to find
+ * Return: add of next char of haystack or NULL
+ */
+char *starts_with(const char *haystack, const char *needle)
+{
+	while (*needle)
+		if (*needle++ != *haystack++)
+			return (NULL);
+	return ((char *)haystack);
+}
+
+/**
+ * _strcat - concatenates two strings
+ * @dest: destination buffer
+ * @src: source buffer
+ * Return: ptr to destination buffer
+ */
+char *_strcat(char *dest, char *src)
+{
+	char *ret = dest;
+
+	while (*dest)
+		dest++;
+	while (*src)
+		*dest++ = *src++;
+	*dest = *src;
+	return (ret);
+}
