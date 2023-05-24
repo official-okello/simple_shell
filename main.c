4ee0b00c99cc6bb796da579f7d3a933840830a9c
official-okello <officialokello@gmail.com>
2023-05-24 22:36:13 +0300

    main.c added

--- main.c
+++ main.c
@@ -1,48 +1,44 @@
 #include "shell.h"
 
 /**
-  * main - main entry point
-  *
-  * Return: 0
-  */
-int main(void)
+ * main - main entry point
+ * @ac: arg count
+ * @av: arg vector
+ *
+ * Return: 0 or 1
+ */
+int main(int ac, char **av)
 {
-	char *line = NULL, **u_tokns = NULL;
-	int w_len = 0, execFlag = 0;
-	size_t line_size = 0;
-	ssize_t line_len = 0;
+	info_t info[] = { INFO_INIT };
+	int fd = 2;
 
-	while (line_len >= 0)
+	asm ("mov %1, %0\n\t"
+			"add $3, %0"
+			: "=r" (fd)
+			: "r" (fd));
+
+	if (ac == 2)
 	{
-		signal(SIGINT, signal_handler);
-		if (isatty(STDIN_FILENO))
-			write(STDOUT_FILENO, "($) ", 4);
-		line_len = getline(&line, &line_size, stdin);
-		if (line_len == -1)
+		fd = open(av[1], O_RDONLY);
+		if (fd == -1)
 		{
-			if (isatty(STDIN_FILENO))
-				write(STDOUT_FILENO, "\n", 1);
-			break;
-		}
-
-		w_len = count_input(line);
-		if (line[0] != '\n' && w_len > 0)
-		{
-			u_tokns = tokenize(line, " \t", w_len);
-			execFlag = execBuiltInCommands(u_tokns, line);
-			if (!execFlag)
+			if (errno == EACCES)
+				exit(126);
+			if (errno == ENOENT)
 			{
-				u_tokns[0] = find(u_tokns[0]);
-				if (u_tokns[0] && access(u_tokns[0], X_OK) == 0)
-					exec(u_tokns[0], u_tokns);
-				else
-					perror("./hsh");
+				_eputs(av[0]);
+				_eputs(": 0: Can't open ");
+				_eputs(av[1]);
+				_eputchar('\n');
+				_eputchar(BUF_FLUSH);
+				exit(127);
 			}
-
-			frees_tokens(u_tokns);
+			return (EXIT_FAILURE);
 		}
+		info->readfd = fd;
 	}
-
-	free(line);
-	return (0);
+	populate_env_list(info);
+	read_history(info);
+	hsh(info, av);
+	return (EXIT_SUCCESS);
 }
